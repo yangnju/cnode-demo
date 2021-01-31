24af723073bf727b75d55464f5cf2dedcf01776f
yangnju <yangfeng611@gmail.com>
2021-01-31 15:54:05 +0800

    first update

---
+++ src/components/PostList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/loading.gif">
+    </div>
+    <div>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<script>``
+export default {
+  name: "PostList.vue",
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    getData() {
+      this.$http.get('https://cnodejs.org/api/v1/topics', {
+        page: 1,
+        limit: 20
+      }).then(res=> {
+        this.isLoading = false
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }
+  },
+  beforeMount() {
+    this.isLoading = true
+    this.getData()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
